661e5699e46faaff9a23464681e2626453ca9480
xRumax <167364@student.uwm.edu.pl>
2023-10-23 12:48:46 +0200

    commit

---
+++ library/library/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    admin = models.BooleanField(False)
+
+
+class Book(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45)
+    author = models.CharField(max_length=45)
+    pub = models.DateField()
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.CharField(max_length=45)
+    rating = models.FloatField()
+    
+
